85c2d387948f15d27a4a32d5f8eebf64346e6a51
shuwenju <jusendia@gmail.com>
2023-06-02 22:44:39 -0400

    fix

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
   app.get('*', (req, res) =>
-    res.sendFile(path.resulve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   ) //anything that is not api routes
 } else {
   app.get('/', (req, res) => {
