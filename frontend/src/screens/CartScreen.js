7d21c10ddf6e6fbe7ada0cb05d0a1dcf40aa830c
Lydia_Shu <93233081+shuwenju@users.noreply.github.com>
2023-06-03 22:19:00 -0400

    Update CartScreen.js
    updated bugs

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -88,10 +88,9 @@ const CartScreen = () => {
         <Card>
           <ListGroup variant='flush'>
             <ListGroup.Item>
-              <h2>
-                Subtotal ({(cartItems.reduce((acc, item) => acc + item.qty), 0)}
-                ) items
-              </h2>
+             <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items
+             </h2>
               $
               {cartItems
                 .reduce((acc, item) => acc + item.qty * item.price, 0)
